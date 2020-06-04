96629c770f891f6950d8072daf1fe1051036c8cf
syhua4 <syhua4@student.monash.edu>
2020-06-04 15:58:14 +1000

    initialize music-app project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import './assets/css/base.scss'
+import VueLazyLoad from 'vue-lazyload'
 
-Vue.config.productionTip = false;
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+Vue.use(VueLazyLoad, {
+  loading: require('assets/images/loading.png')
+})
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        assets: '@/assets',
+        components: '@/components',
+        store: '@/store',
+        views: '@/views',
+        networks: '@/networks'
+      }
+    }
+  }
+}
